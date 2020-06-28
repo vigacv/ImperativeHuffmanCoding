974db656439c39652da91f257f1d0c9d4e2fcdd3
Gabriel <vgabrielcv@gmail.com>
2020-06-28 17:56:10 -0500

    Funciones definidas

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+struct Nodo{
+    int frecuencia;
+    int valor;
+    Nodo* sig;
+};
+
+Nodo* listaFrecuencias = NULL; //Esta lista almacena todos los valores con su frecuencia
+
+void agregarALista(Nodo* cabezaLista, int frec, int valor){
+    //Esta funcion debe agregar un nuevo nodo a la lista de frecuencias
+}
+
+Nodo* contarFrecuencia(int x){
+    return NULL;
+}
+
+void ordenarLista(Nodo* cabezaLista){
+
+}
+
+Nodo* generarArbolHuffman(Nodo* listaFrecuencias){
+    return NULL;
+}
+
+string codificar(int x){
+    return NULL;
+}
+
+int decodificar(string x){
+    return NULL;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
