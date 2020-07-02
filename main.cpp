c5c0cbb347ca4f9ca59d2491db1964f08142e401
NyoByte <20173527@aloe.ulima.edu.pe>
2020-07-02 17:03:31 -0500

    Eliminado funciones innecesarias
    Con hartas ganas de eliminar ese sigNodo

--- main.cpp
+++ main.cpp
@@ -21,12 +21,12 @@ Nodo* CrearNodo(int f, int v){
     newRaiz->valor=v;
     return newRaiz;
 };
+
 //LISTAS
 void MostrarLista(vector<int> v){
     for(int i=0;i<v.size();i++){
         cout<<v[i]<<" ";
     }
-    cout<<endl;
 };
 
 vector<int> OrdenarLista(vector<int> v){
@@ -43,6 +43,7 @@ vector<int> OrdenarLista(vector<int> v){
 };
 
 Nodo* ListaNodos(vector<int> v){
+    v = OrdenarLista(v);
     int frec = 0;
     int valor = v[0];
     Nodo* nodoPrevio = NULL;
@@ -109,28 +110,6 @@ ArbolBB* CrearArbol(Nodo* item, ArbolBB* left, ArbolBB* right){
     return newArbol;
 };
 
-void Agregar(ArbolBB* tree, Nodo* newItem) {
-    if (esVacio(tree)==true) {
-        tree->raiz = newItem;
-    } else {
-        if (newItem->frecuencia < tree->raiz->frecuencia) {
-            if (tree->subArbolIzq==NULL) {
-                tree->subArbolIzq = CrearArbol(newItem, NULL, NULL);
-            } else {
-                Agregar(tree->subArbolIzq, newItem);
-            }
-        } else if (newItem->frecuencia > tree->raiz->frecuencia) {
-            if (tree->subArbolDer==NULL) {
-                tree->subArbolDer = CrearArbol(newItem, NULL, NULL);
-            } else {
-                Agregar(tree->subArbolDer, newItem);
-            }
-        } else {
-            cout<<"Ya existe el item: "<<newItem;
-        }
-    }
-};
-
 vector<ArbolBB*> ListaArboles(Nodo* v){
     vector<ArbolBB*> listaA;
     Nodo* pV = v;
@@ -177,7 +156,6 @@ ArbolBB* GenerarArbolHuffman(vector<ArbolBB*> v){
 };
 
 
-
 bool estaEnArbol(int x, ArbolBB* arbol){
     if(arbol == NULL){
         return false;
@@ -202,13 +180,14 @@ string encontrarCod(int x, ArbolBB* pArbol, string cod){
     }
 }
 
-string codificar(vector<int> x, ArbolBB* arbolHuff){
+// NO PIDE HACER LA CODIFICACIÓN DEL ARBOL
+/*string codificar(vector<int> x, ArbolBB* arbolHuff){
     string cod = "";
     for(int i=0; i<x.size(); i++){
         cod += encontrarCod(x[i],arbolHuff,"");
     }
     return cod;
-};
+};*/
 
 string decodificarCadena(string cod, ArbolBB* arbolHuff){
     string palabra = "";
@@ -229,22 +208,14 @@ string decodificarCadena(string cod, ArbolBB* arbolHuff){
 
 int main(){
     vector<int> val= {1,2,2,1,3,4,2,1,2,3};
-    vector<int> val2 = OrdenarLista(val);
-    MostrarLista(val);
-    cout<<endl;
-    Nodo* lF;
-    lF = ListaNodos(val2);
-    cout<<endl;
-    cout<<"Lista Frecuencia y Valor"<<endl;MostrarListaNodos(lF);
-    cout << endl;
-    vector<ArbolBB*> lA;
-    lA = ListaArboles(lF);
-    ArbolBB* ArbolH=GenerarArbolHuffman(lA);
-    MostrarListaArboles(lA);
+    cout<<"Lista inicial: [ "; MostrarLista(val);
+    cout<<"]"<<endl<<endl;
+    Nodo* lF;   lF = ListaNodos(val);
+    vector<ArbolBB*> lA;    lA = ListaArboles(lF);
+    cout<<"Lista Arbol"<<endl; MostrarListaArboles(lA);
     cout<<"---------"<<endl;
-    cout<<ArbolH->raiz->frecuencia<<":"<<ArbolH->raiz->valor << endl;
-    string valCodificados = codificar(val,ArbolH);
-    cout << valCodificados << endl;
-    cout << "Codigo decodificado: " << decodificarCadena(valCodificados, ArbolH) << endl;
-    //cout<<"\nArbol en preOrden: ";preOrden(pArbol);cout<<endl;
+    ArbolBB* ArbolH=GenerarArbolHuffman(lA);
+    cout<<"Raiz final = "<<ArbolH->raiz->frecuencia<<":"<<ArbolH->raiz->valor << endl;
+    cout<<"Codificar 4 --> "<<encontrarCod(4,ArbolH,"")<<endl;
+    cout << "Decodificar 110 --> "<< decodificarCadena("110", ArbolH) << endl;
 }
